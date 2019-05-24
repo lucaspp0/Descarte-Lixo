27067c7e911b0b59251df8ebb085168516ace378
lucaspp0 <50429022+lucaspp0@users.noreply.github.com>
2019-05-24 19:51:10 -0300

    Create gerenciamento.py

---
+++ gerenciamento.py
@@ -0,0 +1,16 @@
+def lerAlunos():
+  print("lerAlunos")
+
+def lermaterias():
+  print("lerAlunos")
+
+def lerNotas():
+  print("lerAlunos")
+
+def lerTurmas():
+  print("lerAlunos")
+
+notas = []
+materias = []
+alunos = []
+turmas = []
